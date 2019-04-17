b8f210445ea169ae93b356c3df3384a9dbfe5aa5
Nick Young <nick.young@stacktracehq.com>
2019-04-17 10:19:05 +1000

    Fix build on unix systems.

--- src/search/normalize.rs
+++ src/search/normalize.rs
@@ -10,15 +10,21 @@ static WIN_STANDARD_MAX_PATH_LEN: usize = 160;
 
 pub enum NormalizedPath<'a> {
     Untouched(&'a Path),
-    Sliced(&'a str)
+
+    // Only used by Windows, so a warning would normally be emitted on Unix builds
+    #[allow(dead_code)]
+    Sliced(&'a str),
 }
 
 impl NormalizedPath<'_> {
     pub fn new(path: &Path) -> NormalizedPath {
-        if cfg!(windows) {
+        // This block is only included in Windows builds.
+        #[cfg(windows)]
+        {
             if let Some(s) = path.to_str() {
-                if s.starts_with(WIN_EXTENDED_PATH_PREFIX) 
-                    && s.len() < (WIN_STANDARD_MAX_PATH_LEN + WIN_EXTENDED_PATH_PREFIX.len()) {
+                if s.starts_with(WIN_EXTENDED_PATH_PREFIX)
+                    && s.len() < (WIN_STANDARD_MAX_PATH_LEN + WIN_EXTENDED_PATH_PREFIX.len())
+                {
                     let non_extended = &s[WIN_EXTENDED_PATH_PREFIX.len()..];
                     return NormalizedPath::Sliced(non_extended);
                 }
@@ -33,7 +39,7 @@ impl Display for NormalizedPath<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             NormalizedPath::Untouched(path) => path.display().fmt(f),
-            NormalizedPath::Sliced(str) => str.fmt(f)
+            NormalizedPath::Sliced(str) => str.fmt(f),
         }
     }
 }
